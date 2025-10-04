7da2011bf3e54e0b2b85aa723ded443f7f372716
duytan2005 <nguyenduytan20050506@gmail.com>
2025-10-04 20:38:08 +0700

    ch12_1

--- src/java/dao/DatabaseUtil.java
+++ src/java/dao/DatabaseUtil.java
@@ -5,23 +5,28 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtil {
-    private static final String URL = "jdbc:postgresql://localhost:5432/testDB";
-    private static final String USER = "postgres";       // user pgAdmin
-    private static final String PASSWORD = "123456"; // mật khẩu bạn đặt khi cài
+
+    private static final String URL = "jdbc:postgresql://dpg-d3ghdu7fte5s73c5aon0-a.singapore-postgres.render.com:5432/bai8?sslmode=require";
+    private static final String USER = "bai8_user";
+    private static final String PASSWORD = "BZjODcHRjcrLTLLzJDC6Gciq9gcLODUy"; // copy từ Render
 
     public static Connection getConnection() throws SQLException {
         try {
-            Class.forName("org.postgresql.Driver");
+            Class.forName("org.postgresql.Driver"); // load driver
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException e) {
             throw new SQLException("Không tìm thấy driver PostgreSQL!", e);
         }
     }
 
+    // Test kết nối
     public static void main(String[] args) {
-        try {
-            System.out.println("✅ Kết nối PostgreSQL thành công!");
+        try (Connection conn = getConnection()) {
+            if (conn != null) {
+                System.out.println("✅ Kết nối PostgreSQL Render thành công!");
+            }
         } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
